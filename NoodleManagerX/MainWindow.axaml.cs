288234f40fcc520e6b4ad05d54a22b345a65402a
Nova_Max <40432491+tommaier123@users.noreply.github.com>
2020-10-28 19:43:56 +0100

    Completion of tab control, moved window dragging into code behind

--- NoodleManagerX/MainWindow.axaml.cs
+++ NoodleManagerX/MainWindow.axaml.cs
@@ -1,26 +1,89 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Converters;
+using Avalonia.Input;
+using Avalonia.Input.Raw;
 using Avalonia.Markup.Xaml;
 using NoodleManagerX.Models;
+using System;
+using System.Globalization;
 
 namespace NoodleManagerX
 {
     public class MainWindow : Window
     {
+        private Grid blackBar;
+        private MouseDevice mouse;
+        private bool lastleftclick = false;
+        private Point lastclickposition;
+
+
         public MainWindow()
         {
             InitializeComponent();
 
             this.DataContext = new MainViewModel();
+            blackBar = this.FindControl<Grid>("BlackBar");
 
 #if DEBUG
             this.AttachDevTools();
 #endif
+
+
+            Application.Current.InputManager.Process.Subscribe(x =>
+            {
+                if (mouse == null)
+                {
+                    mouse = (MouseDevice)x.Device;
+                }
+                if (x is RawPointerEventArgs rawpointerevent)
+                {
+                    bool leftclick = rawpointerevent.InputModifiers == RawInputModifiers.LeftMouseButton;
+                    if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                    {
+                        Point mouseonclient = desktop.MainWindow.PointToClient(mouse.Position);
+
+                        if (!lastleftclick && leftclick && blackBar.IsPointerOver)//rising edge
+                        {
+                            lastleftclick = true;
+                            lastclickposition = mouseonclient;
+                        }
+                        else if (lastleftclick && leftclick && lastclickposition != null)//hold
+                        {
+                            PixelPoint p = new PixelPoint(mouse.Position.X - (int)lastclickposition.X, mouse.Position.Y - (int)lastclickposition.Y);
+                            desktop.MainWindow.Position = p;
+                        }
+                        else//falling edge
+                        {
+                            lastleftclick = false;
+                        }
+                    }
+                }
+            });
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+    }
+
+    public class EqualsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            return value.ToString() == parameter.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- NoodleManagerX/Models/MainViewModel.cs
+++ NoodleManagerX/Models/MainViewModel.cs
@@ -1,18 +1,11 @@
 ﻿using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Reactive;
-using System.Runtime.CompilerServices;
-using System.Text;
 using Avalonia;
-using Avalonia.Input;
-using System.Timers;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Native;
-using Avalonia.Input.Raw;
+
 
 namespace NoodleManagerX.Models
 {
@@ -22,90 +15,51 @@ namespace NoodleManagerX.Models
         //dotnet publish -c Release -f netcoreapp3.1 -r linux-x64 --self-contained true /p:PublishSingleFile=true
         //dotnet publish -c Release -f netcoreapp3.1 -r osx-x64 --self-contained true /p:PublishSingleFile=true
 
+        [Reactive] public int SelectedTabIndex { get; set; } = 0;
 
-        [Reactive] public string Name { get; set; } = "Name";
-        [Reactive] public string Greeting { get; set; } = "Greetings";
-
-        public ReactiveCommand<Unit, Unit> NameCommand { get; set; }
         public ReactiveCommand<Unit, Unit> MinimizeCommand { get; set; }
-        public ReactiveCommand<Unit, Unit> NormalCommand { get; set; }
-        public ReactiveCommand<Unit, Unit> MaximizeCommand { get; set; }
         public ReactiveCommand<Unit, Unit> ToggleFullscreenCommand { get; set; }
         public ReactiveCommand<Unit, Unit> CloseCommand { get; set; }
-
-
-
-
-        private MouseDevice mouse;
-        private bool lastleftclick = false;
-        private Point lastclickposition;
+        public ReactiveCommand<string, Unit> TabSelectCommand { get; set; }
 
         public MainViewModel()
         {
 
-            MinimizeCommand = ReactiveCommand.Create((Action<Unit>)(x =>
+            MinimizeCommand = ReactiveCommand.Create(() =>
             {
                 if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
                 {
                     desktop.MainWindow.WindowState = WindowState.Minimized;
                 }
-            }));
+            });
 
-            ToggleFullscreenCommand = ReactiveCommand.Create((Action<Unit>)(x =>
+            ToggleFullscreenCommand = ReactiveCommand.Create(() =>
             {
                 if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
                 {
                     if (desktop.MainWindow.WindowState != WindowState.Maximized)
-                    { 
+                    {
                         desktop.MainWindow.WindowState = WindowState.Maximized;
-                }
-                    else if(desktop.MainWindow.WindowState != WindowState.Normal)
+                    }
+                    else if (desktop.MainWindow.WindowState != WindowState.Normal)
                     {
                         desktop.MainWindow.WindowState = WindowState.Normal;
                     }
                 }
-            }));
+            });
 
-            CloseCommand = ReactiveCommand.Create((Action<Unit>)(x =>
+            CloseCommand = ReactiveCommand.Create(() =>
             {
                 if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
                 {
                     desktop.MainWindow.Close();
                 }
-            }));
-
-
-            Application.Current.InputManager.Process.Subscribe(x =>
-            {
-                if (mouse == null)
-                {
-                    mouse = (MouseDevice)x.Device;
-                }
-                if (x is RawPointerEventArgs rawpointerevent)
-                {
-                    bool leftclick = rawpointerevent.InputModifiers == RawInputModifiers.LeftMouseButton;
-                    if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-                    {
-                        Point mouseonclient = desktop.MainWindow.PointToClient(mouse.Position);
-                        bool onwindow = mouseonclient.Y < 70;
-
-                        if (!lastleftclick && leftclick && onwindow)//rising edge
-                        {
-                            lastleftclick = true;
-                            lastclickposition = mouseonclient;
-                        }
-                        else if (lastleftclick && leftclick && lastclickposition != null)//hold
-                        {
-                            PixelPoint p = new PixelPoint(mouse.Position.X - (int)lastclickposition.X, mouse.Position.Y - (int)lastclickposition.Y);
-                            desktop.MainWindow.Position = p;
-                        }
-                        else//falling edge
-                        {
-                            lastleftclick = false;
-                        }
-                    }
-                }
             });
+
+            TabSelectCommand = ReactiveCommand.Create<string>((x =>
+            {
+                SelectedTabIndex = Int32.Parse(x);
+            }));
         }
     }
 }
