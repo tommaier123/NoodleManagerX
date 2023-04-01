78f7801f307a7bac2f5995a52d5ac8d688567b54
Ryan Fredlund <rfredlund13@gmail.com>
2023-04-01 15:53:11 -0500

    Choose SRModsList branch based on beta setting

---
+++ NoodleManagerX/Mods/ModDownloadSource.cs
@@ -0,0 +1,28 @@
+﻿using NoodleManagerX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoodleManagerX.Mods
+{
+    class ModDownloadSource
+    {
+        public static string GetModItemBaseDownloadUrl()
+        {
+            return $"{GetModsBaseUrl()}/Downloads";
+        }
+
+        public static string GetModsBaseUrl()
+        {
+            return $"https://raw.githubusercontent.com/bookdude13/SRModsList/{GetModListBranch()}/SynthRiders";
+        }
+
+        private static string GetModListBranch()
+        {
+            var usingBetas = MainViewModel.s_instance?.settings?.getBetas ?? false;
+            return usingBetas ? "dev" : "master";
+        }
+    }
+}
